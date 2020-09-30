9101a07d1d2f7efe7b51471c3cd98218785bea04
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-09-30 15:24:39 -0400

    Update ifElseRender.js

--- ifElseRender.js
+++ ifElseRender.js
@@ -50,7 +50,6 @@ class MyComponent extends React.Component {
     }));
   }
   render() {
-    // change code below this line
     return (
        <div>
          <button onClick={this.toggleDisplay}>Toggle Display</button>
@@ -60,15 +59,58 @@ class MyComponent extends React.Component {
   }
 };
 
+// Use a Ternary Expression for Conditional Rendering
 
+// condition ? expressionIfTrue : expressionIfFalse;
 
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
 
-
-
-
-
-
-
-
-
-
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+   
+this.state = {
+      userAge: '',
+      input: ''
+    }
+  
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ''
+    });
+  }
+  submit() {
+    this.setState(state => ({
+      userAge: state.input
+    }));
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type='number'
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+        {this.state.userAge === ''
+            ? buttonOne
+            : this.state.userAge >= 18
+              ? buttonTwo
+              : buttonThree}
+      </div>
+    );
+  }
+}
